211169b37a292e1a6f153463dffdafd16fc4bf7d
antiennn <antien2k3@gmail.com>
2024-07-25 22:37:41 +0700

    feat: update letter and number in chessboard

--- src/component/Chessboard.js
+++ src/component/Chessboard.js
@@ -1,15 +1,30 @@
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
-import { OrbitControls, Text } from "@react-three/drei";
+import { OrbitControls,Text, useMatcapTexture } from "@react-three/drei";
 
 const Chessboard = () => {
   const mountRef = useRef(null);
   const boardSize = 5;
   const tileSize = 0.85;
   const borderWidth = 0.1;
-  const [pieces, setPieces] = useState([]);
+  // const [pieces, setPieces] = useState([]);
   const [camera, setCamera] = useState(null);
+  const labels = [];
+  const letters = 'ABCDE';
+  const numbers = '12345';
+  for (let i = 0; i < boardSize; i++) {
+    labels.push(
+      <Text rotation={[-Math.PI / 2, 0, Math.PI/4]} color="black"  position={[i - Math.floor(boardSize / 2) + 0.1, 0.3,  Math.floor(boardSize / 2) + 1]}direction="ltr" fontSize={0.3} key={`bottom-${i}`}>
+        {letters[4-i]}
+      </Text>,
+      
+      <Text rotation={[-Math.PI / 2, 0, Math.PI / 4]}  color="black"  position={[Math.floor(boardSize / 2) + 1, 0.3, i - Math.floor(boardSize / 2) + 0.1]} fontSize={0.3}  key={`left-${i}`}>
+        {numbers[4-i]}
+      </Text>,
+      
+    );
+  }
   useEffect(() => {
     if (mountRef.current) {
       const camera = new THREE.PerspectiveCamera(
@@ -22,7 +37,7 @@ const Chessboard = () => {
       camera.lookAt(0, 0, 0);
       setCamera(camera);
     }
-  }, [mountRef.current]);
+  }, []);
 
   const createTile = useCallback(
     (x, z) => (
@@ -61,6 +76,7 @@ const Chessboard = () => {
         <pointLight position={[10, 10, 10]} />
         <OrbitControls />
         {tiles}
+        {labels}
       </Canvas>
     </div>
   );

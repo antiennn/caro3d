f61499bdc53b5f9095c9b82537f021cf4105641d
antiennn <antien2k3@gmail.com>
2024-08-13 22:03:48 +0700

    feat: add hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useReducer } from 'react';
 import './App.css';
 import Background from './components/Background';
+import Information from './components/Information';
+import Starter from './components/Starter';
 
+import MyContext from './configs/MyContext';
+import MyStateReducer from './hook/MyStateReducer';
 function App() {
+  const [state, dispatch] = useReducer(MyStateReducer, null);
   return (
+    <MyContext.Provider value={[state, dispatch]}>
     <div className="App">
       <header className="App-header">
+        <Starter/>
+        <Information/>
         <Background />
       </header>
     </div>
+    </MyContext.Provider>
   );
 }
 

3e25190102a0d72ae547452a917919637a97d242
antiennn <antien2k3@gmail.com>
2024-08-14 22:13:28 +0700

    feat!: add hook history

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { useReducer } from 'react';
-import './App.css';
-import Background from './components/Background';
-import Information from './components/Information';
-import Starter from './components/Starter';
+import { useReducer } from "react";
+import "./App.css";
+import Background from "./components/Background";
+import Information from "./components/Information";
+import Starter from "./components/Starter";
 
-import MyContext from './configs/MyContext';
-import MyStateReducer from './hook/MyStateReducer';
+import { MyHistoryContext, MyStateContext } from "./configs/MyContext";
+import MyStateReducer from "./hook/MyStateReducer";
+import MyHistoryReducer from "./hook/MyHistory";
 function App() {
   const [state, dispatch] = useReducer(MyStateReducer, null);
+  const [history, dispatchHistory] = useReducer(MyHistoryReducer, []);
   return (
-    <MyContext.Provider value={[state, dispatch]}>
-    <div className="App">
-      <header className="App-header">
-        <Starter/>
-        <Information/>
-        <Background />
-      </header>
-    </div>
-    </MyContext.Provider>
+    <MyStateContext.Provider value={[state, dispatch]}>
+      <MyHistoryContext.Provider value={[history, dispatchHistory]}>
+        <div className="App">
+          <header className="App-header">
+            <Starter />
+            <Information />
+            <Background />
+          </header>
+        </div>
+      </MyHistoryContext.Provider>
+    </MyStateContext.Provider>
   );
 }
 
--- src/components/History.js
+++ src/components/History.js
@@ -1,29 +1,60 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { SlFlag } from "react-icons/sl";
 import { LiaHandshake } from "react-icons/lia";
-import MyContext from "../configs/MyContext";
+import { MyHistoryContext, MyStateContext } from "../configs/MyContext";
 
 export default function History() {
   const [positionY, setPositionY] = useState(384);
-  const [state,dispatch] = useContext(MyContext)
+  const [state, dispatch] = useContext(MyStateContext);
+  const [history, dispatchHistory] = useContext(MyHistoryContext);
+  const myRef = useRef(null);
+  const idle = useRef(null);
+
   useEffect(() => {
-    if(state==="playing"){
+    if (state === "playing") {
       setPositionY(0);
     }
   }, [state]);
+
+  useEffect(() => {
+    if (myRef.current) {
+      myRef.current.scrollTo({
+        top: myRef.current.scrollHeight,
+        behavior: 'smooth'
+      });
+    }
+  }, [history]);
+
   return (
-    <div style={{transform: `translateX(${positionY}px)`,transition: "transform 0.5s ease"}} className="absolute top-[5vh] right-5 w-[25vw] h-[30vh] bg-[#3c373791] rounded-md z-40">
-      <header className="flex justify-start w-full gap-5 ml-5">
+    <div
+      style={{
+        transform: `translateX(${positionY}px)`,
+        transition: "transform 0.5s ease",
+      }}
+      className="absolute top-[10vh] right-5 w-[25vw] h-[30vh] bg-[#3c373791] rounded-md z-40"
+    >
+      <header className="flex justify-center w-full gap-5">
         <item className="flex items-center gap-1 cursor-pointer">
-          <LiaHandshake size={20}/>
+          <LiaHandshake size={20} />
           <p className="text-lg">Suggest Draw</p>
         </item>
         <item className="flex items-center gap-1 cursor-pointer">
-          <SlFlag size={20}/>
+          <SlFlag size={20} />
           <p className="text-lg">Surrender</p>
         </item>
       </header>
       <p className="text-xl font-bold">History</p>
+      <div ref={state == "playing" ? myRef : idle} className="overflow-y-auto h-36 py-2">
+        <div>
+          {history.map((e, idx) => {
+            return (
+              <div key={idx} className="text-base w-full text-start ml-5">
+                {e.date}: {e.content}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }

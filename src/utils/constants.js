c152fdf3e586e5dd76a24354632203c8f86b444a
antiennn <antien2k3@gmail.com>
2024-08-26 12:18:41 +0700

    fix: add function copy board

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,6 +12,21 @@ export const initArray = () => {
   }
   return array;
 };
+export function deepCopyArray3D(array) {
+  let newArray = new Array(array.length);
+
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = new Array(array[i].length);
+    for (let j = 0; j < array[i].length; j++) {
+      newArray[i][j] = new Array(array[i][j].length);
+      for (let k = 0; k < array[i][j].length; k++) {
+        newArray[i][j][k] = array[i][j][k];
+      }
+    }
+  }
+
+  return newArray;
+}
 export const initLocation = () => {
   let array = new Array(5);
 
@@ -70,39 +85,99 @@ export const checkWinner = (board, player) => {
   // Kiểm tra đường chéo trong mặt phẳng xy
   for (let z = 0; z < size; z++) {
     let count1 = 0,
-      count2 = 0;
+      count2 = 0,
+      count3 = 0,
+      count4 = 0,
+      count5 = 0,
+      count6 = 0;
     for (let i = 0; i < size; i++) {
       if (board[i][i][z] === player) count1++;
       else count1 = 0;
       if (board[i][size - i - 1][z] === player) count2++;
       else count2 = 0;
-      if (count1 === winCondition || count2 === winCondition) return true;
+      if (i > 0 && board[i][i - 1][z] === player) count3++;
+      else count3 = 0;
+      if (i < 4 && board[i][i + 1][z] === player) count4++;
+      else count4 = 0;
+      if (i > 0 && board[i][size - i][z] === player) count5++;
+      else count5 = 0;
+      if (i < 4 && board[i][size - i - 2][z] === player) count6++;
+      else count6 = 0;
+      if (
+        count1 === winCondition ||
+        count2 === winCondition ||
+        count3 === winCondition ||
+        count4 === winCondition ||
+        count5 === winCondition ||
+        count6 === winCondition
+      )
+        return true;
     }
   }
 
   // Kiểm tra đường chéo trong mặt phẳng xz
   for (let y = 0; y < size; y++) {
     let count1 = 0,
-      count2 = 0;
+      count2 = 0,
+      count3 = 0,
+      count4 = 0,
+      count5 = 0,
+      count6 = 0;
     for (let i = 0; i < size; i++) {
       if (board[i][y][i] === player) count1++;
       else count1 = 0;
       if (board[i][y][size - i - 1] === player) count2++;
       else count2 = 0;
-      if (count1 === winCondition || count2 === winCondition) return true;
+      if (board[i][y][i - 1] === player && i > 0) count3++;
+      else count3 = 0;
+      if (board[i][y][i + 1] === player && i < 4) count4++;
+      else count4 = 0;
+      if (board[i][y][size - i] === player && i > 0) count5++;
+      else count5 = 0;
+      if (board[i][y][size - i - 2] === player && i < 4) count6++;
+      else count6 = 0;
+      if (
+        count1 === winCondition ||
+        count2 === winCondition ||
+        count3 === winCondition ||
+        count4 === winCondition ||
+        count5 === winCondition ||
+        count6 === winCondition
+      )
+        return true;
     }
   }
 
   // Kiểm tra đường chéo trong mặt phẳng yz
   for (let x = 0; x < size; x++) {
     let count1 = 0,
-      count2 = 0;
+      count2 = 0,
+      count3 = 0,
+      count4 = 0,
+      count5 = 0,
+      count6 = 0;
     for (let i = 0; i < size; i++) {
       if (board[x][i][i] === player) count1++;
       else count1 = 0;
+      if (board[x][i][i - 1] === player && i > 0) count3++;
+      else count3 = 0;
+      if (board[x][i][i + 1] === player && i < 4) count4++;
+      else count4 = 0;
       if (board[x][i][size - i - 1] === player) count2++;
       else count2 = 0;
-      if (count1 === winCondition || count2 === winCondition) return true;
+      if (board[x][i][size - i] === player && i > 0) count5++;
+      else count5 = 0;
+      if (board[x][i][size - i - 2] === player && i < 4) count6++;
+      else count6 = 0;
+      if (
+        count1 === winCondition ||
+        count2 === winCondition ||
+        count3 === winCondition ||
+        count4 === winCondition ||
+        count5 === winCondition ||
+        count6 === winCondition
+      )
+        return true;
     }
   }
 
@@ -166,7 +241,6 @@ function mapYtoNumber(y) {
 }
 
 export const mapToLocationInLayout = (x, z) => {
-  console.log(x + "    " + z);
   return `${mapXtoLetter(x - 1)}${mapYtoNumber(z - 1)}`;
 };
 
@@ -180,3 +254,24 @@ export const getCurrentTime = () => {
   });
   return formattedTime;
 };
+
+export const mapValueToLocation = (x) => {
+  const mappedValue = 3 * x - 5;
+  return mappedValue;
+};
+export function evalute(state) {
+  var score = 0;
+
+  if (checkWinner(state, -1)) {
+    score = 1;
+  } else if (checkWinner(state, 1)) {
+    score = -1;
+  } else {
+    score = 0;
+  }
+
+  return score;
+}
+export function checkAll(state) {
+  return checkWinner(state, -1) || checkWinner(state, 1);
+}

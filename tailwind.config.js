6ea1bf126bb9f3f5b9e4d3df29636f0da08beb66
antiennn <antien2k3@gmail.com>
2024-09-08 21:29:37 +0700

    init: init animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        circle: 'circle-animation 4s linear infinite',
+        "dot-flash": 'dot-flash 3s steps(3, end) infinite',
+      },
+      keyframes: {
+        'circle-animation': {
+          '0%': {
+            transform: 'translate(-50%, -50%) rotate(0deg) translate(40px) rotate(0deg)',
+          },
+          '100%': {
+            transform: 'translate(-50%, -50%) rotate(360deg) translate(40px) rotate(-360deg)',
+          },
+        },
+        'dot-flash': {
+          '0%, 100%': { content: "''" },
+          '33%': { content: "'.'" },
+          '66%': { content: "'..'" },
+          '99%': { content: "'...'" },
+        },
+      },
+    },
     fontFamily: {
       mono: 'monospace',
       segoe:'segoe UI'

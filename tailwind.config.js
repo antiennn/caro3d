754e6ea6e127e656e84734f5d866ab6df47040af
antiennn <antien2k3@gmail.com>
2024-09-09 17:50:53 +0700

    feat: add anomation AI thinking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { rotate } = require('three/examples/jsm/nodes/Nodes.js');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,6 +10,7 @@ module.exports = {
       animation: {
         circle: 'circle-animation 4s linear infinite',
         "dot-flash": 'dot-flash 3s steps(3, end) infinite',
+        "hourglass": 'rotate-hourglass 3s ease infinite',
       },
       keyframes: {
         'circle-animation': {
@@ -23,6 +26,12 @@ module.exports = {
           '33%': { content: "'.'" },
           '66%': { content: "'..'" },
           '99%': { content: "'...'" },
+        },
+        'rotate-hourglass': {
+          '0%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(180deg)' },
+          '90%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(0deg)' },
         },
       },
     },
